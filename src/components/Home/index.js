69cdf9a602c9d29a2d458d4eed996b4bdd296238
charishma-sv <itscharishma@gmail.com>
2021-08-06 23:12:49 -0700

    Home page added buttons

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
+import { Button, Container } from 'react-bootstrap';
 import like from '../../images/like_and_dislike.png';
 function Home() {
   return (
     <Container fluid className="p-0 vh-100 text-center home-bg">
-      <Container>
+      <Container className="mb-4">
         <h1 className="header">Like or Dislike</h1>
         <img alt="" src={like} className="image pt-4" />
+      </Container>
+      <Container className="btn-container mb-4">
+        <Button className="mr-2" variant="dark" size="lg">
+          Login
+        </Button>
+        <Button className="mr-2" variant="dark " size="lg">
+          Signup
+        </Button>
       </Container>
     </Container>
   );
