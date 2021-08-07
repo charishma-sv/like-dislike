b1fb5074d641dc7ed25f83fcc8bb61d3877775ab
charishma-sv <itscharishma@gmail.com>
2021-08-06 22:59:01 -0700

    started Home page

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,7 +1,15 @@
 import React from 'react';
-
+import { Container } from 'react-bootstrap';
+import like from '../../images/like_and_dislike.png';
 function Home() {
-  return <div>Home</div>;
+  return (
+    <Container fluid className="p-0 vh-100 text-center home-bg">
+      <Container>
+        <h1 className="header">Like or Dislike</h1>
+        <img alt="" src={like} className="image pt-4" />
+      </Container>
+    </Container>
+  );
 }
 
 export default Home;
