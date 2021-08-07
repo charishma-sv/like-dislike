de27bc9e31715feeb08cad825e6da79acc86e44c
charishma-sv <itscharishma@gmail.com>
2021-08-07 15:30:58 -0700

    built signup and installed floating label

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,26 +1,31 @@
 import React from 'react';
 import { Button, Container, Form } from 'react-bootstrap';
+import FloatingLabel from 'react-bootstrap-floating-label';
 
 function Login() {
   return (
     <Container className="mw-100 vh-100 d-flex align-items-center">
       <Container className="w-50 h-75">
-        <Form className="light">
+        <Form className="">
           <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" />
+            <FloatingLabel
+              controlId="floatingInput"
+              label="Email address"
+              className="mb-3"
+            >
+              <Form.Control type="email" placeholder="name@example.com" />
+            </FloatingLabel>
             <Form.Text className="text-muted d-none">
               Enter valid email
             </Form.Text>
           </Form.Group>
 
           <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" />
+            <FloatingLabel controlId="floatingPassword" label="Password">
+              <Form.Control type="password" placeholder="Password" />
+            </FloatingLabel>
           </Form.Group>
-          <Form.Group className="mb-3" controlId="formBasicCheckbox">
-            <Form.Check type="checkbox" label="Check me out" />
-          </Form.Group>
+
           <Button variant="primary" type="submit">
             Submit
           </Button>
--- src/components/SignUp/index.js
+++ src/components/SignUp/index.js
@@ -1,7 +1,47 @@
 import React from 'react';
+import { Button, Container, Form } from 'react-bootstrap';
+import FloatingLabel from 'react-bootstrap-floating-label';
 
 function SignUp() {
-  return <div>Signup</div>;
+  return (
+    <Container className="mw-100 vh-100 d-flex align-items-center">
+      <Container className="w-50 h-75">
+        <Form className="">
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <FloatingLabel
+              controlId="floatingInput"
+              label="Name"
+              className="mb-3"
+            >
+              <Form.Control type="text" placeholder="ABCDEF" />
+            </FloatingLabel>
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <FloatingLabel
+              controlId="floatingInput"
+              label="Email address"
+              className="mb-3"
+            >
+              <Form.Control type="email" placeholder="name@example.com" />
+            </FloatingLabel>
+            <Form.Text className="text-muted d-none">
+              Enter valid email
+            </Form.Text>
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <FloatingLabel controlId="floatingPassword" label="Password">
+              <Form.Control type="password" placeholder="Password" />
+            </FloatingLabel>
+          </Form.Group>
+
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </Container>
+    </Container>
+  );
 }
 
 export default SignUp;
