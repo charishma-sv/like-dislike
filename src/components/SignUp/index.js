fae26f630c6d46c3243f26eec733815bba2c51c1
charishma-sv <itscharishma@gmail.com>
2021-08-08 04:31:30 -0700

    fixed floating label

--- src/components/SignUp/index.js
+++ src/components/SignUp/index.js
@@ -6,39 +6,42 @@ function SignUp() {
   const [name, setName] = React.useState('');
   const [email, setEmail] = React.useState('');
   const [password, setPassword] = React.useState('');
+  console.log('email, name,password', email, name, password);
   const onChangeHandler = (event) => {
     console.log('inside change event', event);
-    const { name, value } = event.target;
-    if (name === 'userName') {
+    const { id, name, value } = event.target;
+    console.log('id,', id);
+    if (id === 'name-input') {
       console.log('inside user name');
       setName(value);
-    } else if (name === 'userEmail') {
+    } else if (id === 'email-input') {
       console.log('inside user email');
       setEmail(value);
-    } else if (name === 'userPassword') {
+    } else if (id === 'password-input') {
       setPassword(value);
     }
   };
-  console.log('email, name', email, name);
   return (
     <Container className="mw-100 vh-100 d-flex align-items-center">
       <Container className="w-50 h-75">
         <Form className="">
           <Form.Group className="mb-3">
-            <FloatingLabel label="Name" className="mb-3">
-              <Form.Control
-                type="text"
-                name="userName"
-                value={name}
-                placeholder="name@example.com"
-                onChange={(event) => onChangeHandler(event)}
-              />
+            <FloatingLabel
+              label="Name"
+              className="mb-3"
+              id="name"
+              name="userName"
+              onChange={(event) => onChangeHandler(event)}
+              value={name}
+            >
+              <Form.Control type="text" placeholder="name@example.com" />
             </FloatingLabel>
           </Form.Group>
           <Form.Group className="mb-3">
             <FloatingLabel
               label="Email address"
               className="mb-3"
+              id="email"
               name="userEmail"
               value={email}
               onChange={(event) => onChangeHandler(event)}
@@ -51,14 +54,15 @@ function SignUp() {
           </Form.Group>
 
           <Form.Group className="mb-3">
-            <FloatingLabel label="Password">
-              <Form.Control
-                type="password"
-                placeholder="Password"
-                name="userPassword"
-                value={password}
-                onChange={(event) => onChangeHandler()}
-              />
+            <FloatingLabel
+              label="password"
+              className="mb-3"
+              id="password"
+              name="userPassword"
+              value={password}
+              onChange={(event) => onChangeHandler(event)}
+            >
+              <Form.Control type="password" placeholder="name@example.com" />
             </FloatingLabel>
           </Form.Group>
 
