7dfda99c3d7488f619be795c9496dd66e77c0088
charishma-sv <itscharishma@gmail.com>
2021-08-08 23:47:40 -0700

    reverted changes

--- src/components/Random/RandomImage.js
+++ src/components/Random/RandomImage.js
@@ -1,24 +1,41 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
+import pic from '../../images/patrick-tomasso-QMDap1TAu0g-unsplash.jpg';
 import dislike from '../../images/black-thumb-dislike-product.png';
 import like from '../../images/like-thumb-black-product.png';
+import { randomPic } from '../../unsplash';
+import { addPic } from '../../firebase';
 function RandomImage(props) {
+  const { user } = props;
+  const [picture, setPicture] = React.useState(pic);
   const [likeCount, setLikeCount] = React.useState(0);
+  const [picId, setPicId] = React.useState('');
+
+  const getRandomPic = async () => {
+    const { photo, id } = await randomPic();
+    setPicture(photo);
+    setPicId(id);
+    console.log('photoId', id);
+  };
+
+  React.useEffect(() => {
+    getRandomPic();
+  }, []);
 
   const handleLike = () => {
-    props.randomPic();
+    getRandomPic();
     setLikeCount(likeCount + 1);
-    props.addPicture();
+    addPic(user, picId);
   };
 
   const handleDisLike = () => {
-    props.randomPic();
+    getRandomPic();
   };
   console.log('liked count', likeCount);
   return (
     <Container>
       <Container fluid className="random-img p-0">
-        <img src={props.picture} alt="" className="h-100" />
+        <img src={picture} alt="" className="h-100" />
       </Container>
       <Container className="mt-4">
         <img
