395baf2cfa8f9d1289cb8d471497f513d31abce3
charishma-sv <itscharishma@gmail.com>
2021-08-17 20:40:36 +0000

    small change in home page

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -6,7 +6,7 @@ function Home() {
   return (
     <Container fluid className="p-0 vh-100 text-center home-bg">
       <Container className="mb-4">
-        <h1 className="header">Like or Dislike</h1>
+        <h1 className="header">Like - Dislike</h1>
         <img alt="" src={like} className="image pt-4" />
       </Container>
       <Container className="btn-container mb-4">
